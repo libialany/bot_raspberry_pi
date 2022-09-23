c7752fd30d2f713a700434b8beeacfc268a8acdb
libialany <elibialany@gmail.com>
2022-09-23 19:42:59 +0100

    end

--- basics/app_make_functions.py
+++ basics/app_make_functions.py
@@ -1,5 +1,5 @@
 from discord.ext import commands
-import discord, os
+import discord, os, requests, random, string
 from dotenv import load_dotenv
 load_dotenv()
 TOKEN=os.getenv('TOKEN')
@@ -17,7 +17,27 @@ async def foo(msg):
 async def bar(msg):
     await msg.send(f"FOO")
 
+@bot.command(name="cat",help="quiero gatos")
+async def cat(msg):
+    request_url = 'https://api.thecatapi.com/v1/images/search'
+    r = requests.get(request_url).json()
+    await msg.send(f" "+r[0]["url"])
+
 @bot.command(name="iloveyou",help="iloveyou responds with Love")
 async def iloveyou(msg):
     await msg.send(f"i love you too")
+
+@bot.command(name="generate_passwd",help="Random String Generator")
+async def generate_passwd(msg):
+    await msg.send(f"".join(random.choice(string.ascii_letters + string.punctuation) for x in range(20)))
+
+
+@bot.command(name="embed",help=".")
+async def embed(ctx):
+    embed=discord.Embed(title=ctx.author.display_name, url="https://realdrewdata.medium.com/", description="".join(random.choice(string.ascii_letters + string.punctuation) for x in range(20)), color=0xFF5733)
+    request_url = 'https://api.thecatapi.com/v1/images/search'
+    r = requests.get(request_url).json()
+    embed.set_thumbnail(url=r[0]["url"])
+    await ctx.send(embed=embed)
+
 bot.run(TOKEN)
